eb99ee3e867a2711a2114cc82c522cbdb87a139c
Kory Taborn <kory.taborn@gmail.com>
2020-01-08 01:52:26 -0600

    broken down components and added menu styling

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './menu-item.styles.scss'
+
+const MenuItem = ({ title, imageUrl, size }) => {
+  return (
+    <div className={`${size} menu-item`}>
+      <div
+        className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }} />
+      <div className="content">
+        <h1 className="title">{title}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  )
+}
+
+MenuItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+  size: PropTypes.string,
+}
+
+export default MenuItem
