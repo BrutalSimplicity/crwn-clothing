84d45f976686c9cab450358ee015baaec02b2617
Kory Taborn <kory.taborn@gmail.com>
2020-02-03 22:20:01 -0600

    wrong method for sign-in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,6 +3,7 @@ import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth } from '../../firebase/firebase.utils';
 
 export default class SignIn extends Component {
   constructor() {
@@ -14,13 +15,24 @@ export default class SignIn extends Component {
     };
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
 
-    this.setState({
-      email: '',
-      password: ''
-    });
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(
+        email,
+        password
+      );
+  
+      this.setState({
+        email: '',
+        password: ''
+      });
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   handleChange = event => {
