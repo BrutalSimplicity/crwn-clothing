2eea4ba6adb8b56a3bd9591e497cd003a5e46a9d
Kory Taborn <kory.taborn@gmail.com>
2020-03-18 22:56:04 -0500

    added cart-icon and cart-dropdown components plus cart-reducer

--- src/App.js
+++ src/App.js
@@ -50,7 +50,11 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({currentUser: user.currentUser});
+const mapStateToProps = ({ 
+  user: { currentUser },
+  }) => ({
+      currentUser,
+    });
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
