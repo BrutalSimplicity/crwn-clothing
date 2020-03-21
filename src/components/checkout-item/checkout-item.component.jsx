f514e718c9e8515e972b16075e0fb23d2a8facf3
Kory Taborn <kory.taborn@gmail.com>
2020-03-21 12:58:07 -0500

    wip

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,24 +1,35 @@
 import React from 'react'
 import { connect } from 'react-redux';
-import { removeItem } from '../../redux/cart/cart.actions';
+import { removeItem, clearItemFromCart, addItem } from '../../redux/cart/cart.actions';
 
 import './checkout-item.styles.scss';
+import { createStructuredSelector } from 'reselect';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 
-const CheckoutItem = ({ item, dispatch }) => {
-    const { id, name, quantity, imageUrl, price } = item;
+const CheckoutItem = ({ item, dispatch, cartItems }) => {
+    const { name, quantity, imageUrl, price } = item;
+
     return (
         <div className="checkout-item">
             <div className="image-container">
                 <img src={imageUrl} alt="item"/>
             </div>
             <span className="name">{name}</span>
-            <span className="quantity">{quantity}</span>
+            <span className="quantity">
+                <div onClick={() => dispatch(removeItem(item))} className='arrow'>&#10094;</div>
+                <span className='value'>{quantity}</span>
+                <div onClick={() => dispatch(addItem(item))} className='arrow'>&#10095;</div>
+            </span>
             <span className="price">{price}</span>
             <div 
-                onClick={() => dispatch(removeItem(id))}
+                onClick={() => dispatch(clearItemFromCart(item))}
                 className="remove-button">&#10005;</div>
         </div>
     )
 }
 
-export default connect()(CheckoutItem);
+const mapStateToProps = createStructuredSelector({
+    cartItems: selectCartItems
+})
+
+export default connect(mapStateToProps)(CheckoutItem);
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -6,3 +6,10 @@ export const addItemToCart = (items, newItem) => {
 
   return [...items, { ...newItem, quantity: 1 }];
 }
+
+export const removeItemFromCart = (items, dropItem) => {
+  return dropItem.quantity > 1 ?
+    items.map(item => item.id === dropItem.id ? { ...item, quantity: item.quantity - 1 } : item)
+    :
+    items.filter(item => item.id !== dropItem.id);
+}
